4bcd7f6e401a551f0b8ab8115de7a5e6c97893b6
syntax bender <mail@syntaxbender.com>
2020-07-17 22:33:47 +0300

    fixed bug on multiple table.

--- sbTableSorter.js
+++ sbTableSorter.js
@@ -12,23 +12,18 @@ var sbTableSorter = {
 			bufferHeader += "<th><div style=\"cursor:pointer;display:inline-block;vertical-align:top;\" class=\"sbTableSortButton\" data-element=\""+elementID+"\" data-column=\""+i+"\">"+header[i]+"</div><span style=\"font-size:8px;margin-top:-4px;\" class=\"fa-stack fa-lg\"><i class=\"fa fa-chevron-up fa-stack-1x\"></i><i style=\"padding-top: 8px;\" class=\"fa fa-chevron-down fa-stack-1x\"></i></span></th>";
 		}
 		bufferHeader += "</tr>";
-		console.log("count :x "+this.rowCounts[elementID]);
-		console.log((start+this.rowCounts[elementID]));
+
 		for(var i=start;i<(start+this.rowCounts[elementID]);i++){
 			if(array[i] == null) break;
 			bufferTable += "<tr>";
 			for(var i2=0;i2<(header.length);i2++){
-				console.log(array[i][i2]);
 				bufferTable += "<td>"+array[i][i2]+"</td>";
 			}
 			bufferTable += "</tr>";
 		}
-		//console.log(elementID);
 		var totalPageNum = Math.ceil(array.length/this.rowCounts[elementID]);
 		var bufferPaginationButtons = "";
 		for(var i=1;i<=totalPageNum;i++){
-			//console.log("active : "+this.activePages[elementID]);
-			//console.log("inci: "+i);
 			if(i == this.activePages[elementID]){
 				bufferPaginationButtons += "<div style=\"display:inline-block; margin:0px 5px; background:blue;padding:5px;border-radius:8px;\" class=\"sbTablePageButton\" data-element=\""+elementID+"\" data-pagenum=\""+i+"\">"+i+"</div>";
 			}else{
@@ -40,14 +35,12 @@ var sbTableSorter = {
 	},
 	sort: function(elementID,column){
 		if(this.sortHolder[elementID] == column){
-			console.log(1);
 			delete this.sortHolder[elementID];
 			sortedArr = this.arrays[elementID].reverse();
 		}else{
 			sortedArr = this.arrays[elementID].sort(function (a, b){
 			  return a[column].localeCompare(b[column]);
 			});
-			console.log(sortedArr);
 			this.arrays[elementID] = sortedArr;
 			this.sortHolder[elementID] = column;
 		}
@@ -62,15 +55,18 @@ var sbTableSorter = {
 	search: function(elementID,search){
 
 	},
-	run: function(options){
-		//for ile arraydaki eleman array lengthi al verilen length(kolon sayisi) ile check et eşlemeşmeyen varsa array hata bas. 
-		//header length ile  arraydaki eleman array lengthi kontrol et aynı değilse hata bas
+	addItem: function(options){
 		this.arrays[options.elementID] = options.array;
 		this.headers[options.elementID] = options.headers;
 		this.rowCounts[options.elementID] = options.rowCount;
 		this.activePages[options.elementID] = 1;
-		this.renderTable(options.headers,options.elementID,options.array,0);
- 
+	},
+	run: function(){
+		//for ile arraydaki eleman array lengthi al verilen length(kolon sayisi) ile check et eşlemeşmeyen varsa array hata bas. 
+		//header length ile  arraydaki eleman array lengthi kontrol et aynı değilse hata bas
+		for(index in this.headers){
+			this.renderTable(this.headers[index],index,this.arrays[index],0);
+		}
 		$(document).on('click', ".sbTableSortButton", function(event){
 			var elementID = $(this).data("element");
 			var column = $(this).data("column");
