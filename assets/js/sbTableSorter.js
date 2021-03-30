be9a71cef7574f6bb91b7e66f042e9b43925e1a7
Syntax Bender <mail@syntaxbender.com>
2021-03-31 01:31:28 +0300

    test

--- assets/js/sbTableSorter.js
+++ assets/js/sbTableSorter.js
@@ -1,16 +1,18 @@
 var sbTableSorter = {
-	arrays: {},
+	arrays:{},
+	sourceArrays:{},
 	headers:{},
 	rowCounts:{},
 	sortHolder:{},
 	activePages: {},
+	searchInterval: {},
 	renderTable: function(header,elementID,array,start){
 		var tableBody = document.createElement("tbody");
 		let tableHeaderRow = document.createElement("tr");
 		for(let i=0;i<(header.length);i++){
 			let tableCell = document.createElement("th");
 			let headerDiv = document.createElement("div");
-			headerDiv.className="sbTableSortButton";
+			headerDiv.setAttribute("class","sbTableSortButton");
 			headerDiv.setAttribute("data-element",elementID);
 			headerDiv.setAttribute("data-column",i);
 			headerDiv.innerHTML = header[i]+"<img src=\"assets/img/arrow.svg\" style=\"width:13px;margin:0px 3px;\">";
@@ -29,11 +31,70 @@ var sbTableSorter = {
 			}
 			tableBody.appendChild(tableRow);
 		}
-		var totalPageNum = Math.ceil(array.length/this.rowCounts[elementID]);
+		var lastPage = Math.ceil(array.length/this.rowCounts[elementID]);
 		var paginationButtons = document.createElement("div");
-		for(let i=1;i<=totalPageNum;i++){
+        buttonCount = 10;
+        let step = 1;
+        let directionIndicator = 0;
+        let currentPage = parseInt(this.activePages[elementID]);
+        let slip = 0;
+        let indicatorStopper = 0;
+        let paginationButton = document.createElement("div");
+        paginationButton.setAttribute("class","sbTablePageButton");
+        paginationButton.classList.add("sbTableActivePageButton");
+        paginationButton.setAttribute("data-element",elementID);
+        paginationButton.setAttribute("data-pagenum",currentPage);
+        paginationButton.innerHTML = currentPage;
+        paginationButtons.innerHTML = paginationButton.outerHTML;
+        paginationButton.setAttribute("class","sbTablePageButton");
+        
+       	// 0 negative direction
+		// 1 pozitive direction
+		console.log("##############################");
+        while(step <= buttonCount-1){
+            console.log(step);
+            let addition = (indicatorStopper == 0) ? Math.ceil(step/2) : step;
+            if(directionIndicator == 0){
+                let pageNum = currentPage-addition+slip;
+            	console.log(pageNum+"a");
+
+                if(indicatorStopper == 1 && pageNum<1){
+					console.log("sxxa");
+                	break;
+                }
+                if(pageNum<1){
+                    indicatorStopper = 1;
+                    directionIndicator = (directionIndicator+1)%2;
+                    slip = (Number.isInteger(step/2) === false) ? (Math.ceil(step/2)-1) : 0;
+                    continue;
+                }
+                paginationButton.setAttribute("data-pagenum",pageNum);
+                paginationButton.innerHTML = pageNum;
+                paginationButtons.innerHTML = paginationButton.outerHTML+paginationButtons.innerHTML;
+            }else{
+                let pageNum = currentPage+addition-slip;
+            	console.log(".."+pageNum+"b");
+
+                if(indicatorStopper == 1 && pageNum>lastPage){
+					console.log("sxxb");
+					break;
+                }
+                if(pageNum>lastPage){
+                    indicatorStopper = 1;
+                    directionIndicator = (directionIndicator+1)%2;
+                    slip = (Number.isInteger(step/2)) ? (Math.ceil(step/2)-1) : 0;
+                    continue;
+                }
+                paginationButton.setAttribute("data-pagenum",pageNum);
+                paginationButton.innerHTML = pageNum;
+                paginationButtons.innerHTML = paginationButtons.innerHTML+paginationButton.outerHTML;
+            }
+            if(indicatorStopper == 0) directionIndicator = (directionIndicator+1)%2;
+            step++;
+        }
+		/*for(let i=1;i<=totalPageNum;i++){
 			let paginationButton = document.createElement("div");
-			paginationButton.className = "sbTablePageButton";
+			paginationButton.setAttribute("class","sbTablePageButton");
 			paginationButton.setAttribute("data-element",elementID);
 			paginationButton.setAttribute("data-pagenum",i);
 			paginationButton.innerHTML = i;
@@ -41,7 +102,8 @@ var sbTableSorter = {
 				paginationButton.classList.add("sbTableActivePageButton");
 			}
 			paginationButtons.appendChild(paginationButton);
-		}
+		}*/
+
 		document.getElementById(elementID+"-pagination").innerHTML = paginationButtons.innerHTML;
 		document.getElementById(elementID).innerHTML = tableBody.innerHTML;
 	},
@@ -74,7 +136,16 @@ var sbTableSorter = {
 		window.addEventListener('load', event => {
 			for(index in this.headers){
 				this.renderTable(this.headers[index],index,this.arrays[index],0);
+				var searchDiv = document.getElementById(index+"-search");
+				if(searchDiv){
+					let searchInput = document.createElement("input");
+					searchInput.setAttribute("class","sbTableSearchInput");
+					searchInput.setAttribute("data-element",index);
+					searchInput.setAttribute("type","text");
+					searchDiv.innerHTML = searchInput.outerHTML;
+				}
 			}
+
 			window.addEventListener('click', event => {
 				if(event.target.closest(".sbTableSortButton")){
 					const elementID = event.target.closest(".sbTableSortButton").getAttribute("data-element");
@@ -86,9 +157,54 @@ var sbTableSorter = {
 					sbTableSorter.changePage(elementID,pagenum);
 				}
 			});
+			window.addEventListener('keyup', event => {
+				if(event.target.closest(".sbTableSearchInput")){
+					var searchWord = event.target.value;
+					var elementID = event.target.getAttribute("data-element");
+					if(this.searchInterval[elementID]) clearInterval(this.searchInterval[elementID]);
+					this.searchInterval[elementID] = setInterval(function(){ sbTableSorter.intervalDone(elementID,searchWord) }, 200);
+				}
+			});
 		});
 	},
-	/*search: function(elementID,search){ // it will be next release.
-
-	},*/
-} 
+	escapeRegExp: function(string) {
+		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	},
+	intervalDone: function(elementID,searchWord){
+		this.search(elementID,searchWord);
+		clearInterval(this.searchInterval[elementID]);
+	},
+	search: function(elementID,searchWord){
+		if(searchWord.length>=3){
+			if(this.sourceArrays[elementID]){
+				var sourceArray = this.sourceArrays[elementID];
+			}else{
+				var sourceArray = this.arrays[elementID];
+				this.sourceArrays[elementID] = this.arrays[elementID];
+			}
+			var searchedArray = [];
+			for(let i=0; i<sourceArray.length;i++){
+				for(let i2=0; i2<sourceArray[i].length;i2++){
+					console.log(sourceArray[i][i2]+"");
+					var res = new RegExp("^.*"+this.escapeRegExp(searchWord)+".*$").exec(sourceArray[i][i2]);
+					if(res){
+						console.log("ok");
+						searchedArray.push(sourceArray[i]);
+						break;
+					}
+				}
+			}
+			if(searchedArray.length<1){
+				this.arrays[elementID] = this.sourceArrays[elementID];
+			}else{
+				this.arrays[elementID] = searchedArray;
+				this.activePages[elementID] = 1;
+				this.renderTable(this.headers[elementID],elementID,searchedArray,0);
+			}
+		}else if(searchWord.length==0) {
+			if(this.sourceArrays[elementID]) this.arrays[elementID] = this.sourceArrays[elementID];
+			this.activePages[elementID] = 1;
+			this.renderTable(this.headers[elementID],elementID,this.arrays[elementID],0);
+		}
+	}
+}
